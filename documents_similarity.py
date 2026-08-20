024dbb5b74b67d8dc9f8d5408be39f1c36304894
Kunal Duvey <kunal04duvey@gmail.com>
2026-08-20 22:59:53 +0530

    embeddings

---
+++ documents_similarity.py
@@ -0,0 +1,34 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+
+embedding = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+documents = [
+    "Delhi is the capital of India",
+    "delhi is capital of india with good infra as delhi need good infra",
+    
+    "Kolkata is the capital of West Bengal",
+    "Paris is the capital of France"
+]
+
+query = "what is the capital of India"
+
+doc_embeddings = embedding.embed_documents(documents)
+query_embedding = embedding.embed_query(query)
+
+similarities = cosine_similarity(
+    [query_embedding],
+    doc_embeddings
+)
+
+scores = similarities[0]
+
+index , score = sorted(list(enumerate(scores)),key = lambda x : x[1])[-1]
+
+print(query)
+
+print(documents[index])
+
+print("similarity score :" ,score)
